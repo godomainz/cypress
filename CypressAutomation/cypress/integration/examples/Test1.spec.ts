0a1ccc3fc84697329a3d120a15614f857c57f4e3
Akila Mendis <akila@oneit.com.au>
2022-11-28 15:22:43 +0530

    15. Cypress inbuilt plugin in testRunner to generate locators

--- CypressAutomation/cypress/integration/examples/Test1.spec.ts
+++ CypressAutomation/cypress/integration/examples/Test1.spec.ts
@@ -2,5 +2,6 @@ describe('My First Test', () => {
   it('Does not do much!', () => {
     const url = "https://rahulshettyacademy.com/seleniumPractise/#/";
     cy.visit(url);
+    cy.get(".search-keyword").type("ca");
   })
 })
