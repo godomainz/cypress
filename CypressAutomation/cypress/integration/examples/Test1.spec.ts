93c78db3eab4a14ab35bf716cb64657a6aeb28a4
Akila Mendis <akila@oneit.com.au>
2022-11-28 15:43:16 +0530

    17. Handling Invisible elements with Cypress by understanding logs

--- CypressAutomation/cypress/integration/examples/Test1.spec.ts
+++ CypressAutomation/cypress/integration/examples/Test1.spec.ts
@@ -4,6 +4,6 @@ describe('My First Test', () => {
     cy.visit(url);
     cy.get(".search-keyword").type("ca");
     cy.wait(2000);
-    cy.get(".product").should("have.length", 4);
+    cy.get(".product:visible").should("have.length", 4);
   })
 })
