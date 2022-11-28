4c3ef4a8745efcf212db5568fc59a526559592c9
Akila Mendis <akila@oneit.com.au>
2022-11-28 15:35:28 +0530

    16. Basic Assertion in writing the tests with Cypress

--- CypressAutomation/cypress/integration/examples/Test1.spec.ts
+++ CypressAutomation/cypress/integration/examples/Test1.spec.ts
@@ -3,5 +3,7 @@ describe('My First Test', () => {
     const url = "https://rahulshettyacademy.com/seleniumPractise/#/";
     cy.visit(url);
     cy.get(".search-keyword").type("ca");
+    cy.wait(2000);
+    cy.get(".product").should("have.length", 4);
   })
 })
