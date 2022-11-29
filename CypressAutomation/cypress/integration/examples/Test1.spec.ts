1c5cd1c6f52a54251a609cb0a7a62b2b9888c542
Akila Medis <akila@oneit.com.au>
2022-11-29 14:19:27 +0530

    19. Understanding get and find commands with Cypress

--- CypressAutomation/cypress/integration/examples/Test1.spec.ts
+++ CypressAutomation/cypress/integration/examples/Test1.spec.ts
@@ -4,6 +4,10 @@ describe('My First Test', () => {
     cy.visit(url);
     cy.get(".search-keyword").type("ca");
     cy.wait(2000);
-    cy.get(".product:visible").should("have.length", 4);
+    // cy.get(".product:visible").should("have.length", 4);
+    // Parent child chaining
+    cy.get(".products").find(".product").should("have.length", 4);
+
+    cy.get(".products").find(".product").eq(1).contains("ADD TO CART").click();
   })
 })
