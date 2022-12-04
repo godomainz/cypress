783943f246a7da0f7c261af45deb3b0d369283e8
Akila Mendis <akila@oneit.com.au>
2022-12-04 22:01:30 +0530

    22. Understanding the difference between Jquery methods and cypress commands

--- CypressAutomation/cypress/integration/examples/Test1.spec.ts
+++ CypressAutomation/cypress/integration/examples/Test1.spec.ts
@@ -1,5 +1,7 @@
+import promisify from 'cypress-promise';
+
 describe('My First Test', () => {
-  it('Does not do much!', () => {
+  it('Does not do much!', async() => {
     const url = "https://rahulshettyacademy.com/seleniumPractise/#/";
     cy.visit(url);
     cy.get(".search-keyword").type("ca");
@@ -9,7 +11,7 @@ describe('My First Test', () => {
     cy.get(".products").find(".product").should("have.length", 4);
 
     // cy.get(".products").find(".product").eq(1).contains("ADD TO CART").click();
-    cy.get(".products").find(".product").each(($el, index, list) => {
+    cy.get(".products").find(".product").each(async($el, index, list) => {
       const element = cy.wrap($el);
       const txt = $el.find(".product-name").text();
       const vegitable = "Carrot";
@@ -17,6 +19,10 @@ describe('My First Test', () => {
         element.find("button").click();
       }
     });
-
+    const logo = await promisify(cy.get('.brand'));
+    cy.log(logo.text());
+    // cy.get('.brand').then((el) => {
+    //   cy.log(el.text());
+    // });
   })
 })
