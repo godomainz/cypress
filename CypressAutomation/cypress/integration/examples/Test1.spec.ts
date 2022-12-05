14fbbccd03ae8c8ca02c222dafba7b8702498c1f
Akila Mendis <akila@oneit.com.au>
2022-12-05 10:44:41 +0530

    23. Handing Async promises with Cypress

--- CypressAutomation/cypress/integration/examples/Test1.spec.ts
+++ CypressAutomation/cypress/integration/examples/Test1.spec.ts
@@ -8,10 +8,11 @@ describe('My First Test', () => {
     cy.wait(2000);
     // cy.get(".product:visible").should("have.length", 4);
     // Parent child chaining
-    cy.get(".products").find(".product").should("have.length", 4);
+    cy.get(".products").as("productLocator");
+    cy.get("@productLocator").find(".product").should("have.length", 4);
 
-    // cy.get(".products").find(".product").eq(1).contains("ADD TO CART").click();
-    cy.get(".products").find(".product").each(async($el, index, list) => {
+    // cy.get("@productLocator").find(".product").eq(1).contains("ADD TO CART").click();
+    cy.get("@productLocator").find(".product").each(async($el, index, list) => {
       const element = cy.wrap($el);
       const txt = $el.find(".product-name").text();
       const vegitable = "Carrot";
@@ -21,6 +22,7 @@ describe('My First Test', () => {
     });
     const logo = await promisify(cy.get('.brand'));
     cy.log(logo.text());
+    cy.get('.brand').should("have.text","GREENKART")
     // cy.get('.brand').then((el) => {
     //   cy.log(el.text());
     // });
