c1e45fce82c2a95ecde9854081611f6bce0bd789
Akila Mendis <akila@oneit.com.au>
2022-12-05 16:49:10 +0530

    29. Handling Dynamic dropdowns with each command Iteration

--- CypressAutomation/cypress/integration/examples/Dropdown.spec.ts
+++ CypressAutomation/cypress/integration/examples/Dropdown.spec.ts
@@ -1,11 +1,21 @@
+import promisify from 'cypress-promise';
 describe('My Second Dropdown Suite', () => {
     it('My Dropdown Test case', async() => {
-      const url = "https://rahulshettyacademy.com/AutomationPractice/";
-      cy.visit(url);
+        const url = "https://rahulshettyacademy.com/AutomationPractice/";
+        cy.visit(url);
 
-      //Static Dropdown
-      cy.get("select").select("option2").should("have.value","option2");
+        //Static Dropdown
+        cy.get("select").select("option2").should("have.value","option2");
 
-
+        // Dynamic Dropdown
+        cy.get('#autocomplete').type("ind");
+        const country = "India";
+        cy.get(".ui-menu-item > div").each(($el)=>{
+            if($el.text()===country){
+                const element = cy.wrap($el);
+                element.click();
+            }
+        });
+        cy.get('#autocomplete').should("have.value", country);
     });
   });
