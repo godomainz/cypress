b2d780189b186a6c08a8852b5430734f0443a423
Akila Mendis <akila@oneit.com.au>
2022-12-06 10:01:24 +0530

    30. Handling Visible and invisible elements using Assertions in Cypress

---
+++ CypressAutomation/cypress/integration/examples/Visibility.spec.ts
@@ -0,0 +1,11 @@
+describe('My Second Third Suite', () => {
+    it('My Third Test case', async() => {
+      const url = "https://rahulshettyacademy.com/AutomationPractice/";
+      cy.visit(url);
+      cy.get("#displayed-text").should("be.visible");
+      cy.get("#hide-textbox").click();
+      cy.get("#displayed-text").should("not.be.visible");
+      cy.get("#show-textbox").click();
+      cy.get("#displayed-text").should("be.visible");
+    });
+  });
