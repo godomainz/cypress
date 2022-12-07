e8b4d2d82283ef17c0d99273d262ea7589a1cef9
Akila Mendis <akila@oneit.com.au>
2022-12-07 16:46:52 +0530

    36. Handling Web tables with Cypress using each command

---
+++ CypressAutomation/cypress/integration/examples/Table.spec.ts
@@ -0,0 +1,14 @@
+describe('My ChildTab Suite', () => {
+    it('My ChildTab Test case',() => {
+        const url = "https://rahulshettyacademy.com/AutomationPractice/";
+        cy.visit(url);
+        const subject = 'Master Selenium Automation in simple Python Language';
+        const price = '25';
+        cy.get('table[name="courses"] > tbody td:nth-child(2)').each(($el, index, $list)=>{
+            if($el.text().includes(subject)){
+                cy.wrap($el).next().should("have.text", price);
+            }
+        });
+        
+    });
+});
