4273465c8d3bc1d025fa03ea4336885df63424b5
Akila Mendis <akila@oneit.com.au>
2022-12-07 17:08:27 +0530

    38. Handling Mouse hover popus using Cypress

---
+++ CypressAutomation/cypress/integration/examples/MouseHover.spec.ts
@@ -0,0 +1,10 @@
+describe('My ChildTab Suite', () => {
+    it('My ChildTab Test case',() => {
+        const url = "https://rahulshettyacademy.com/AutomationPractice/";
+        cy.visit(url);
+        cy.get('.mouse-hover-content').invoke('show');
+        cy.contains('Top').click();
+        // cy.contains('Top').click({force: true});
+        cy.url().should('include', 'top')
+    });
+});
