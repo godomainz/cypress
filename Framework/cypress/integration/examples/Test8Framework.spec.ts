42705e78e1c93d5b6481fd7c859096b613f793d4
Akila Mendis <akila@oneit.com.au>
2022-12-08 15:51:39 +0530

    45. Understand how fixtures work in driving data

--- Framework/cypress/integration/examples/Test8Framework.spec.ts
+++ Framework/cypress/integration/examples/Test8Framework.spec.ts
@@ -1,12 +1,16 @@
+let data;
 describe('My first Framework Suite', () => {
     before(()=>{
+        cy.fixture('example').then((jsondata)=>{
+            data = jsondata;
+        });
         const url = "https://rahulshettyacademy.com/angularpractice/";
         cy.visit(url);
     });
 
     it('My Visibility case', () => {
-        cy.get('input[name="name"]:nth-child(1)').type("Akila");
-        cy.get('#exampleFormControlSelect1').select("Male")
+        cy.get('input[name="name"]:nth-child(1)').type(data.name);
+        cy.get('#exampleFormControlSelect1').select(data.gender);
 
     });
   });
