6076c569af01238f7b8324b90a959283c29015a9
Akila Mendis <akila@oneit.com.au>
2022-12-10 09:41:24 +0530

    48. Parameterizing the test Data from Json files using each command

--- Framework/cypress/integration/examples/Commands.spec.ts
+++ Framework/cypress/integration/examples/Commands.spec.ts
@@ -1,13 +1,18 @@
 describe('My first Framework Suite', () => {
+    let data;
     before(()=>{
+        cy.fixture('example').then((jsondata)=>{
+            data = jsondata;
+        });
         const url = "https://rahulshettyacademy.com/angularpractice/";
         cy.visit(url);
     });
 
     it('My Commands case', () => {
         cy.get(':nth-child(2) > .nav-link').click();
-        const phoneName = "Blackberry";
-        cy.selectProduct(phoneName);
+        data.productNames.forEach((val)=>{
+            cy.selectProduct(val);
+        });
     });
 
 });
