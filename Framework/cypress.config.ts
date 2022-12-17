194945d031ea3ae1c6e3a459ea8da15410ebdb0c
Akila Mendis <akila@oneit.com.au>
2022-12-17 18:31:35 +0530

    added allure report

--- Framework/cypress.config.ts
+++ Framework/cypress.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "cypress";
-
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+declare const allure: any;
 export default defineConfig({
   defaultCommandTimeout: 8000,
   env: {
@@ -16,6 +17,8 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
+      allureWriter(on, config);
+      return config;
     },
     specPattern: "./cypress/integration/examples/*.ts",
     projectId: "1"
