e7e3db33950fa7bf040c6bc4aac4981f3bc1160c
Akila Mendis <akila@oneit.com.au>
2022-12-21 23:40:07 +0530

    79. Intercepting HTTP request details to test Security Scenarios

---
+++ Framework/cypress/integration/examples/HTTPRequestSecurity.spec.ts
@@ -0,0 +1,27 @@
+import { AppDemoPage } from "./pageObjects/AppDemoPage";
+
+describe('My Mock HTTP Suite', () => {
+    let appDemoPage:AppDemoPage;
+    const domain = Cypress.env('url');
+
+    before(()=>{
+        appDemoPage = new AppDemoPage();
+        const url = domain + "/angularAppdemo/";
+        cy.visit(url);
+    });
+
+    it('My Mock HTTP case', () => {
+        let authorName = "shetty";
+        let url = domain + "/Library/GetBook.php?AuthorName=" + authorName;
+        cy.intercept('GET', url, (req)=>{
+            authorName = "malhotra";
+            url = domain + "/Library/GetBook.php?AuthorName=" + authorName;
+            req.url = url;
+            req.continue((res)=>{
+                expect(res.statusCode).to.equal(404);
+            });
+        }).as("dummyUrl");
+        appDemoPage.getVirtualLibrary().click();
+        cy.wait("@dummyUrl");
+    });
+});
