d7cdaa169c97cb3f35ff84a08c207e14ea6c109f
Akila Mendis <akila@oneit.com.au>
2022-12-22 20:36:44 +0530

    85. How to save the Login tokens in browser Local storage using Cypress

--- Framework/cypress/integration/examples/Section_17/JWTToken.spec.ts
+++ Framework/cypress/integration/examples/Section_17/JWTToken.spec.ts
@@ -1,10 +1,18 @@
 describe('My Mock JWT Token Suite', () => {
 
     before(()=>{
-        
+        cy.loginAPI();
     });
 
     it('My JWT Token case', () => {
-       
+        const token = Cypress.env("token");
+        const url = Cypress.env("url") + "/client/";
+        cy.visit(url, 
+            { 
+                onBeforeLoad : (window)=> {
+                    window.localStorage.setItem("token", token);
+                } 
+            }
+        );
     });
 });
